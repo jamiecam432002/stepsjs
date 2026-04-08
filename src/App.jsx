e26434023ee821b5eff4e809236bf709f3278810
James Cameron <jamiecam43@gmail.com>
2026-04-08 15:05:13 -0400

    final 'steps' of steps mini lesson

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,50 @@ const messages = [
 
 export default function App() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
 		if (step === 1) return;
-		setStep((step) => step - 1);
+		setStep((prev) => prev - 1);
 	}
 
 	function handleNext() {
 		if (step === 3) return;
-		setStep((step) => step + 1);
+		setStep((prev) => prev + 1);
+	}
+
+	function handleClose() {
+		setIsOpen((open) => !open);
 	}
 
 	return (
-		<div className='steps'>
-			<div className='numbers'>
-				<div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-				<div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-				<div className={`${step >= 3 ? 'active' : ''}`}>3</div>
-			</div>
-			<p className='message'>
-				Step {step}: {messages[step - 1]}
-			</p>
+		<>
+			<button className='close' onClick={handleClose}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className='steps'>
+					<div className='numbers'>
+						<div className={step >= 1 ? 'active' : ''}>1</div>
+						<div className={step >= 2 ? 'active' : ''}>2</div>
+						<div className={step >= 3 ? 'active' : ''}>3</div>
+					</div>
+					<p className='message'>
+						Step {step}: {messages[step - 1]}
+					</p>
 
-			<div className='buttons'>
-				<button className='bg-[#7950f2] text-white' onClick={handlePrevious}>
-					Previous
-				</button>
-				<button className='bg-[#7950f2] text-white' onClick={handleNext}>
-					Next
-				</button>
-			</div>
-		</div>
+					<div className='buttons'>
+						<button
+							className='bg-[#7950f2] text-white'
+							onClick={handlePrevious}>
+							Previous
+						</button>
+						<button className='bg-[#7950f2] text-white' onClick={handleNext}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
